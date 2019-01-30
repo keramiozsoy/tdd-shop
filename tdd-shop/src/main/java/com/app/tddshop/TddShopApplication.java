b87de581de23d9c00093c5eb7dc4bafb14964006
kerami <keramiozsoy@hotmail.com>
2019-01-30 19:47:16 +0300

    project init

---
+++ tdd-shop/src/main/java/com/app/tddshop/TddShopApplication.java
@@ -0,0 +1,14 @@
+package com.app.tddshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddShopApplication.class, args);
+	}
+
+}
+
