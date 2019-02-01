9644e21a09f33081ea9793435f67008dfa951a44
kerami <keramiozsoy@hotmail.com>
2019-02-01 23:03:18 +0300

    shopping cart test - add one product

--- tdd-shop/src/main/java/com/app/tddshop/shoppingcart/ShoppingCart.java
+++ tdd-shop/src/main/java/com/app/tddshop/shoppingcart/ShoppingCart.java
@@ -1,9 +1,17 @@
 package com.app.tddshop.shoppingcart;
 
+import com.app.tddshop.domain.Product;
+
 public class ShoppingCart {
 
+	private int listSize = 0;
+	
 	public int size() {
-		return 0;
+		return listSize;
+	}
+
+	public void add(Product product) {
+		listSize = 1;
 	}
 
 }
--- tdd-shop/src/test/java/com/app/tddshop/test/ShoppingCartTest.java
+++ tdd-shop/src/test/java/com/app/tddshop/test/ShoppingCartTest.java
@@ -1,21 +1,36 @@
 package com.app.tddshop.test;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.app.tddshop.domain.Product;
 import com.app.tddshop.shoppingcart.ShoppingCart;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ShoppingCartTest {
 
+	private ShoppingCart cart = null;
+	private Product product = null;
+	
+	@Before
+	public void initTest() {
+		 cart = new ShoppingCart();
+		 product = new Product();
+	}
+	
 	@Test
 	public void t1_zero_size_list() {
-		ShoppingCart cart = new ShoppingCart();
 		Assert.assertEquals("Listenin büyüklüğü sıfır değildir.", 0, cart.size());
 	}
 	
+	@Test
+	public void t2_test_list_size_after_adding_one_product() {
+		cart.add(product);
+		Assert.assertEquals("Listenin büyüklüğü bir değildir.", 1, cart.size());
+	}
 }
